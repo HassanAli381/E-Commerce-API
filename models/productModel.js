2098c24ecf7559d1a53cc5101b485cd7672b5a06
Al-Hassan Ali <alhassanali599@gmail.com>
2024-03-03 00:34:06 +0200

    Adding ratingsQuantity field

--- models/productModel.js
+++ models/productModel.js
@@ -38,6 +38,10 @@ const productSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+    ratingsQuantity:{
+        type: Number,
+        default: 0 
+    },
     usersWishlisted: [{
         type: mongoose.Types.ObjectId,
         ref: 'User'
